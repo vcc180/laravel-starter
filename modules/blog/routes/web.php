3c3dbe6dfbf8a58e15b96429727c0affb5de493f
Starter <starter@example.com>
2026-07-10 20:30:19 -0300

    refactor(rbac): proteger rotas/admin via CheckPermission com permissões alinhadas às seeds

--- modules/blog/routes/web.php
+++ modules/blog/routes/web.php
@@ -4,27 +4,34 @@ use Illuminate\Support\Facades\Route;
 use Modules\Blog\Http\Controllers\CategoryController;
 use Modules\Blog\Http\Controllers\PostController;
 use Modules\Blog\Http\Controllers\TagController;
+use App\Http\Middleware\CheckPermission;
 
 Route::middleware(['web', 'auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/blog', [PostController::class, 'index'])->name('blog.index');
-    Route::get('/blog/create', [PostController::class, 'create'])->name('blog.create');
-    Route::post('/blog', [PostController::class, 'store'])->name('blog.store');
-    Route::get('/blog/{post}', [PostController::class, 'show'])->name('blog.show');
-    Route::get('/blog/{post}/edit', [PostController::class, 'edit'])->name('blog.edit');
-    Route::put('/blog/{post}', [PostController::class, 'update'])->name('blog.update');
-    Route::delete('/blog/{post}', [PostController::class, 'destroy'])->name('blog.destroy');
+    Route::middleware(CheckPermission::class.':blog.posts.*')->group(function () {
+        Route::get('/blog', [PostController::class, 'index'])->name('blog.index');
+        Route::get('/blog/create', [PostController::class, 'create'])->name('blog.create');
+        Route::post('/blog', [PostController::class, 'store'])->name('blog.store');
+        Route::get('/blog/{post}', [PostController::class, 'show'])->name('blog.show');
+        Route::get('/blog/{post}/edit', [PostController::class, 'edit'])->name('blog.edit');
+        Route::put('/blog/{post}', [PostController::class, 'update'])->name('blog.update');
+        Route::delete('/blog/{post}', [PostController::class, 'destroy'])->name('blog.destroy');
+    });
 
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    Route::middleware(CheckPermission::class.':blog.categories.*')->group(function () {
+        Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+        Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+        Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    });
 
-    Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
-    Route::get('/tags/create', [TagController::class, 'create'])->name('tags.create');
-    Route::post('/tags', [TagController::class, 'store'])->name('tags.store');
-    Route::get('/tags/{tag}/edit', [TagController::class, 'edit'])->name('tags.edit');
-    Route::put('/tags/{tag}', [TagController::class, 'update'])->name('tags.update');
-    Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
+    Route::middleware(CheckPermission::class.':blog.tags.*')->group(function () {
+        Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
+        Route::get('/tags/create', [TagController::class, 'create'])->name('tags.create');
+        Route::post('/tags', [TagController::class, 'store'])->name('tags.store');
+        Route::get('/tags/{tag}/edit', [TagController::class, 'edit'])->name('tags.edit');
+        Route::put('/tags/{tag}', [TagController::class, 'update'])->name('tags.update');
+        Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -5,30 +5,35 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\ExampleController;
 use App\Http\Controllers\Admin\LocaleController;
+use App\Http\Middleware\CheckPermission;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['web'])->prefix('admin')->name('admin.')->group(function () {
     Route::middleware(['auth', 'admin'])->group(function () {
         Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
-        Route::resource('examples', App\Http\Controllers\Admin\ExampleController::class);
-        Route::resource('articles', App\Http\Controllers\Admin\ArticleController::class);
-        Route::resource('locales', App\Http\Controllers\Admin\LocaleController::class)->except(['show']);
-        Route::post('locales/{locale}/set-default', [App\Http\Controllers\Admin\LocaleController::class, 'setDefault'])->name('locales.set-default');
-        Route::resource('permissions', App\Http\Controllers\Admin\PermissionController::class);
-        Route::resource('roles', App\Http\Controllers\Admin\RoleController::class);
-        Route::get('users', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
-        Route::get('users/create', [App\Http\Controllers\Admin\UserController::class, 'create'])->name('users.create');
-        Route::post('users', [App\Http\Controllers\Admin\UserController::class, 'store'])->name('users.store');
-        Route::get('users/{user}', [App\Http\Controllers\Admin\UserController::class, 'edit'])->name('users.edit');
-        Route::put('users/{user}', [App\Http\Controllers\Admin\UserController::class, 'update'])->name('users.update');
-        Route::delete('users/{user}', [App\Http\Controllers\Admin\UserController::class, 'destroy'])->name('users.destroy');
-        Route::put('users/{user}/roles', [App\Http\Controllers\Admin\UserRoleController::class, 'update'])->name('users.roles.update');
-        Route::get('users/{user}/roles', [App\Http\Controllers\Admin\UserRoleController::class, 'edit'])->name('users.roles.edit');
+        Route::resource('examples', App\Http\Controllers\Admin\ExampleController::class)->middleware(CheckPermission::class.':example.*');
+        Route::resource('articles', App\Http\Controllers\Admin\ArticleController::class)->middleware(CheckPermission::class.':article.*');
+        Route::resource('locales', App\Http\Controllers\Admin\LocaleController::class)->except(['show'])->middleware(CheckPermission::class.':locale.*');
+        Route::post('locales/{locale}/set-default', [App\Http\Controllers\Admin\LocaleController::class, 'setDefault'])->name('locales.set-default')->middleware(CheckPermission::class.':locale.update');
+        Route::resource('permissions', App\Http\Controllers\Admin\PermissionController::class)->middleware(CheckPermission::class.':permission.*');
+        Route::resource('roles', App\Http\Controllers\Admin\RoleController::class)->middleware(CheckPermission::class.':role.*');
+        Route::middleware(CheckPermission::class.':user.*')->group(function () {
+            Route::get('users', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
+            Route::get('users/create', [App\Http\Controllers\Admin\UserController::class, 'create'])->name('users.create');
+            Route::post('users', [App\Http\Controllers\Admin\UserController::class, 'store'])->name('users.store');
+            Route::get('users/{user}', [App\Http\Controllers\Admin\UserController::class, 'edit'])->name('users.edit');
+            Route::put('users/{user}', [App\Http\Controllers\Admin\UserController::class, 'update'])->name('users.update');
+            Route::delete('users/{user}', [App\Http\Controllers\Admin\UserController::class, 'destroy'])->name('users.destroy');
+            Route::put('users/{user}/roles', [App\Http\Controllers\Admin\UserRoleController::class, 'update'])->name('users.roles.update');
+            Route::get('users/{user}/roles', [App\Http\Controllers\Admin\UserController::class, 'edit'])->name('users.roles.edit');
+        });
 
-        Route::get('repository', [App\Http\Controllers\Admin\RepositoryController::class, 'index'])->name('repository.index');
-        Route::post('repository/{type}/{slug}/install', [App\Http\Controllers\Admin\RepositoryController::class, 'install'])->name('repository.install');
-        Route::delete('repository/{type}/{slug}/uninstall', [App\Http\Controllers\Admin\RepositoryController::class, 'uninstall'])->name('repository.uninstall');
+        Route::middleware(CheckPermission::class.':repository.*')->group(function () {
+            Route::get('repository', [App\Http\Controllers\Admin\RepositoryController::class, 'index'])->name('repository.index');
+            Route::post('repository/{type}/{slug}/install', [App\Http\Controllers\Admin\RepositoryController::class, 'install'])->name('repository.install');
+            Route::delete('repository/{type}/{slug}/uninstall', [App\Http\Controllers\Admin\RepositoryController::class, 'uninstall'])->name('repository.uninstall');
+        });
     });
 
     Route::get('/login', [App\Http\Controllers\Admin\Auth\LoginController::class, 'showLoginForm'])->name('login');
