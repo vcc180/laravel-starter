c4001b7e18f83ef4eeae529a0c40de4b0996aff5
Starter <starter@example.com>
2026-07-10 19:53:20 -0300

    feat(blog): registrar listeners de exemplo no BlogServiceProvider

--- modules/blog/src/Providers/BlogServiceProvider.php
+++ modules/blog/src/Providers/BlogServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Blog\Providers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +13,23 @@ class BlogServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->loadViewsFrom(base_path('modules/blog/src/resources/views'), 'blog');
+
+        $hooks = $this->app->make(\Core\Contracts\HookInterface::class);
+
+        $hooks->addAction('blog.post.created', function ($post) {
+            Log::info('Post criado.', ['post_id' => $post->id]);
+        });
+
+        $hooks->addAction('blog.post.deleted', function ($post) {
+            Log::info('Post removido.', ['post_id' => $post->id]);
+        });
+
+        $hooks->addAction('blog.category.updated', function ($category) {
+            Log::info('Categoria atualizada.', ['category_id' => $category->id]);
+        });
+
+        $hooks->addAction('blog.tag.deleted', function ($tag) {
+            Log::info('Tag removida.', ['tag_id' => $tag->id]);
+        });
     }
 }
